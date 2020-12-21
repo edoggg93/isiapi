64dcfb93ebc40449706b2c74ac60947a20c47bd3
Edoardo <edoardogullo@libero.it>
2020-12-21 15:02:49 +0100

    logging

--- app/src/main/java/com/isi/isiapi/MainActivity.java
+++ app/src/main/java/com/isi/isiapi/MainActivity.java
@@ -1,18 +1,7 @@
 package com.isi.isiapi;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-
-import com.isi.isiapi.general.classes.Department;
-import com.isi.isiapi.general.classes.Operator;
-import com.isi.isiapi.general.classes.OrderGuest;
-import com.isi.isiapi.general.classes.Report;
-import com.isi.isiapi.isicashier.HttpRequest;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
